f725d09cf55d15188883f772ab26d7f8a14eef84
Mamadou Sarr <mbayelel@gmail.com>
2022-08-29 12:36:24 +0000

    finished tp1

--- index.js
+++ index.js
@@ -6,3 +6,52 @@ for (let i = 0; i < blocsLength; i++) {
     blocs[i].classList.toggle("active");
   });
 }
+
+/* blocs.map((bloc) => {
+  btn.addEventListener("click", function () {
+    bloc.classList.toggle("active");
+  });
+}); */
+
+let inputs = document.getElementsByClassName("form-control");
+const data = {};
+const infos = [];
+let inputsLength = inputs.length;
+for (let i = 0; i < inputsLength; i++) {
+  inputs[i].addEventListener("keyup", function () {
+    const id = inputs[i].id;
+    const value = inputs[i].value;
+    data[id] = value;
+  });
+}
+
+const resetInputs = () => {
+  for (let i = 0; i < inputsLength; i++) {
+    inputs[i].value = "";
+  }
+};
+//regex for email
+const regexEmail = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+
+const submitBtn = document.getElementById("submit");
+submitBtn.addEventListener("click", function (e) {
+  e.preventDefault();
+  if (
+    (data.nom && data.prenom && data.email && data.message) ||
+    regexEmail.test(data.email)
+  ) {
+    submitBtn.disabled = false;
+  } else {
+    submitBtn.disabled = true;
+  }
+  infos.push(data);
+  const result = document.getElementById("result");
+  resetInputs();
+  result.innerHTML = `
+    <p>Prenom: ${data.prenom}</p>
+    <p>Nom: ${data.nom}</p>
+    <p>Email: ${data.email}</p>
+    <p>Message: ${data.message}</p>
+    `;
+  console.log(infos);
+});
